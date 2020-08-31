275328e15b71f5a4c57c18c49aeeb3f38807f47f
auritiwdesign <juancamilo.auriti@gmail.com>
2020-08-31 09:09:09 +0200

    update
    upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+var slideshow = document.getElementById('slideshow');
+var slides = slideshow.getElementsByTagName('img');
+var index = 0;
+
+function nxSlide() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+function prSlide() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+
+var slideTX = document.getElementById('slideTX');
+var slidesTX = slideTX.getElementsByTagName('div');
+var i = 0;
+
+function nxSlideTX() {
+    slidesTX[i].classList.remove('active');
+    i = (i + 1) % slidesTX.length;
+    slidesTX[i].classList.add('active');
+}
+function prSlideTX() {
+    slidesTX[i].classList.remove('active');
+    i = (i - 1 + slidesTX.length) % slidesTX.length;
+    slidesTX[i].classList.add('active');
+}
